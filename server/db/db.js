fdc98dec7c1575e809a39a61fca710095d608911
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Pro.local>
2021-07-30 00:29:25 -0700

    ticket1 completed

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:2002101174postgres@localhost:5432/messengerapp", {
   logging: false
 });
+
+db.authenticate().then(function(errors) {console.log(errors)});
 
 module.exports = db;
